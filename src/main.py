bde2637c291d053a1657f4d487dc885123918b4b
Sarai613 <120747707+Sarai613@users.noreply.github.com>
2023-01-14 13:42:50 -0600

    Agregamos intake

--- src/main.py
+++ src/main.py
@@ -6,10 +6,14 @@ controller = Controller()
 left_wheel = Motor(Ports.PORT3, GearSetting.RATIO_18_1, False)
 right_wheel = Motor(Ports.PORT2, GearSetting.RATIO_18_1, True)
 back_wheel = Motor(Ports.PORT12, GearSetting.RATIO_18_1, True)
+intake = Motor(Ports.PORT11, GearSetting.RATIO_18_1, True)
+above_wheel = Motor(Ports.PORT4, GearSetting.RATIO_18_1, True)
 
+INTAKE_VELOCITY = 100
+TURN_SENSIBILITY = 0.6
 while True:
     drive = controller.axis3.position()
-    turn = controller.axis1.position()
+    turn = controller.axis1.position() * TURN_SENSIBILITY
     lateral = controller.axis4.position()
     left_wheel_power = drive + turn
     right_wheel_power = drive - turn
@@ -17,3 +21,17 @@ while True:
     back_wheel.spin(FORWARD, back_wheel_power, PERCENT)
     left_wheel.spin(FORWARD, left_wheel_power, PERCENT)
     right_wheel.spin(FORWARD, right_wheel_power, PERCENT)
+
+    if controller.buttonL2.pressing():
+        intake.spin(FORWARD, INTAKE_VELOCITY, PERCENT)
+    elif controller.buttonR2.pressing():
+        intake.spin(REVERSE, INTAKE_VELOCITY, PERCENT)
+    else:
+        intake.set_velocity(0, PERCENT)
+
+    if controller.buttonL1.pressing():
+        above_wheel.spin(FORWARD, INTAKE_VELOCITY, PERCENT)
+    elif controller.buttonR1.pressing():
+        above_wheel.spin(REVERSE, INTAKE_VELOCITY, PERCENT)
+    else:
+        above_wheel.set_velocity(0, PERCENT)
